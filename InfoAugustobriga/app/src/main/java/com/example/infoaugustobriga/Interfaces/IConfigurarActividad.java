c2fecbfeca0472a509fe0ac1e8c38c0240c57a78
Adrian <moraloamg@gmail.com>
2022-12-15 12:22:11 +0100

    prueba pull

--- InfoAugustobriga/app/src/main/java/com/example/infoaugustobriga/Interfaces/IConfigurarActividad.java
+++
@@ -1,9 +0,0 @@
-package com.example.infoaugustobriga.Interfaces;
-
-import android.content.res.Configuration;
-
-public interface IConfigurarActividad extends IModosClaroOscuro{
-    void cambiarDiaNoche(Configuration newConfig);
-    void identificarElementosInterfaz(Configuration newConfig);
-    void activarBotones();
-}
--- InfoAugustobriga/app/src/main/java/com/example/infoaugustobriga/Interfaces/IModosClaroOscuro.java
+++
@@ -1,8 +0,0 @@
-package com.example.infoaugustobriga.Interfaces;
-
-public interface IModosClaroOscuro {
-    //Constantes de modo claro y oscuro
-    //los modos de la clase Configuration no funcionan bien, por eso los pongo manualmente
-    int MODO_CLARO = 17;
-    int MODO_OSCURO = 33;
-}
